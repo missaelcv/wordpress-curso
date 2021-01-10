4e610f4114742292344673f1c3c178e71471a20b
MisaWeb <elbilpapi@gmail.com>
2021-01-10 15:07:16 -0400

    Nuevo Wordpress

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
+
+// Agregamos la URL base de nuestra API
+axios.defaults.baseURL = 'http://localhost:80/wordpress-api/appmcv/wp-json/';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
